ebe9f99191f5eb1971dc6ee105bb65713b71d4df
Trigbluy <guilhermemuniz.fg@gmail.com>
2024-01-13 18:11:15 -0300

    anim correct

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,6 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 var last_direction = "right"
+var acting = false;
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var anim = $AnimationPlayer as AnimationPlayer
 
@@ -12,7 +13,6 @@ func _physics_process(delta):
 	# Add the gravity.
 	#if not is_on_floor():
 	#	velocity.y += gravity * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept"):
 		if last_direction == "left":
@@ -23,18 +23,19 @@ func _physics_process(delta):
 			anim.play("stop_up")
 		elif last_direction == "down":
 			anim.play("stop_down")
-		
+		acting = true
 		time_magic()
 	
-		if Input.is_action_just_pressed("parry"):
-			if last_direction == "left":
-				anim.play("parring_left")
-			elif last_direction == "right":
-				anim.play("parring_right")
-			elif last_direction == "up":
-				anim.play("parring_up")
-			elif last_direction == "down":
-				anim.play("parring_down")
+	if Input.is_action_just_pressed("parry"):
+		if last_direction == "left":
+			anim.play("parring_left")
+		elif last_direction == "right":
+			anim.play("parring_right")
+		elif last_direction == "up":
+			anim.play("parring_up")
+		elif last_direction == "down":
+			anim.play("parring_down")
+		acting = true
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction_x = Input.get_axis("ui_left", "ui_right")
@@ -64,8 +65,17 @@ func _physics_process(delta):
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
-	if !direction_x and !direction_y:
+	if !direction_x and !direction_y and !acting:
 		anim.stop()
+		
 
 func time_magic ():
 	print("time_magic()")
+
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name.begins_with("stop"):
+		acting = false
+	if anim_name.begins_with("parring"):
+		acting = false
