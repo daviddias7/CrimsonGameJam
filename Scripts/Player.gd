431fa502de6709faa27a4d6e20c5da3c5b643245
Trigbluy <guilhermemuniz.fg@gmail.com>
2024-01-13 16:16:21 -0300

    player andando animado

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -20,10 +20,27 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction_x = Input.get_axis("ui_left", "ui_right")
+	var direction_y = Input.get_axis("ui_down", "ui_up")
+	
+	if direction_x:
+		velocity.x = direction_x * SPEED
+		if direction_x < 0:
+			anim.play("walk_left")
+		else:
+			anim.play("walk_right")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if direction_y:
+		velocity.y = -direction_y * SPEED
+		if !velocity.x:
+			if direction_y > 0:
+				anim.play("walk_up")
+			else:
+				anim.play("walk_down")
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
+	print(velocity.x)
 	move_and_slide()
