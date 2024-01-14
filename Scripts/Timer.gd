b64f7e28db8191865e1ef709d34c4c8e849ce6ee
Trigbluy <guilhermemuniz.fg@gmail.com>
2024-01-13 22:52:49 -0300

    ball animation

--- Scripts/Timer.gd
+++ Scripts/Timer.gd
@@ -5,7 +5,7 @@ const snowball_scene = preload("res://snowball.tscn")
 
 func _on_timeout():
 	var snowball_instance = snowball_scene.instantiate()
-	snowball_instance.position = get_parent().position
-	snowball_instance.get_node("RigidBody2D").linear_velocity = BALL_SPEED * (get_parent().get_parent().get_node("Player").position - snowball_instance.position).normalized()
+	snowball_instance.position = get_parent().get_parent().position
+	snowball_instance.get_node("RigidBody2D").linear_velocity = BALL_SPEED * (get_parent().get_parent().get_parent().get_node("Player").position - snowball_instance.position).normalized()
 	add_child(snowball_instance)
 	wait_time = randf_range(0.1, 1)
