48ae9466b7504e498004c18202bbadb5c1482f24
David <david.dias02@usp.br>
2024-01-14 22:30:21 -0300

    Spawn enemy first commit

--- Scripts/node_2d.gd
+++ Scripts/node_2d.gd
@@ -4,7 +4,17 @@ extends Node2D
 @export var mod = Color.hex(0xffffffff)
 
 @onready var tiles = $TileMap as TileMap
+const enemy_scene = preload("res://prefabs/enemy_1.tscn")
 
+@onready var enemy_spawner = $EnemySpawner as Timer
+@onready var player = $Player as CharacterBody2D
 
 func _process(delta):
 	tiles.modulate = mod
+
+func _on_enemy_spawner_timeout():
+	var enemy_instance = enemy_scene.instantiate()
+	var x = player.position.x + 1152 if player.position.x + 1152 < 750 else player.position.x - 1152
+	var y = player.position.y + 648 if player.position.y + 648 < 650 else player.position.y - 648
+	enemy_instance.position = Vector2(x, y)
+	add_child(enemy_instance)
