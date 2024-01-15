3b8190b8091f7441ebf09f5e94c077171cb3b069
David <david.dias02@usp.br>
2024-01-14 23:12:04 -0300

    autoloads

---
+++ autoloads/MusicController.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var music = load("res://autoloads/winter-wind.mp3")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func play_music():
+	$Music.stream = music
+	$Music.play()
